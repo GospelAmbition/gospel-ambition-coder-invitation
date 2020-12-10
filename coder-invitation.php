bda73122f91e9fcc615ab8a941606a664a1f22c9
Chris Chasm <chris@chasm.solutions>
2020-12-10 11:56:37 -0700

    Updated message width.

--- coder-invitation.php
+++ coder-invitation.php
@@ -47,16 +47,25 @@ If you are on this site, you are likely a follower of Jesus.
 If so, join us!
 
 We are technologists who are using all available means in
-order to lift up Jesus before the world. Our prayers and hopes
-are to saturate the world with multiplying disciples in our
-generation. Never before in history could a small group of
-people instantly reach and influence the world with little
-more than vision, passion, some coffee, and a laptop.
+order to lift up Jesus before the world. Our prayers and
+hopes are to saturate the world with multiplying
+disciples in our generation. Never before in history could
+a small group of people instantly reach and influence the
+world with little more than vision, passion, some coffee,
+and a laptop.
 
 Reach out to us here and join us:
-https://zume.community (A community of disciple making movement practitioners)
-https://disciple.tools (A community of technologists building a disciple making movement software)
-https://kingdom.training (A community of faith pioneers using media outreach to find seekers on the internet)
+
+https://zume.community
+A community of disciple making movement practitioners.
+
+https://disciple.tools
+A community of technologists building
+a disciple making movement software
+
+https://kingdom.training
+A community of faith pioneers using media outreach to
+find seekers on the internet
         `)
     </script>
     <?php
